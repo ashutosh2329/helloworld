ea9eb889fea8da40fed322c3c88abf872225a5bb
ashutosh2329 <sonutiwari2329@gmail.com>
2019-11-26 09:52:57 -0800

    made changes to setting files

--- helloworld/helloworld/settings.py
+++ helloworld/helloworld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nnrz-x6i=f@6$tqsktz&rw&7d#r)9r)t0%705^--^kwvsu&m@n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
